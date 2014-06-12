a1e479c43220cdb816226da3941cfc8021ccad0c
fpaxton <farispaxton@Faris-Paxtons-MacBook.local>
2014-06-12 07:02:54 -0500

    Finish Ryu
    Added fight music; added background img; I tried to smooth out the
    event handlers so multiple images of Ryu would not display at the same
    time.

--- js/app.js
+++ js/app.js
@@ -1,27 +1,38 @@
 $(document).ready(function(){
+	playStage();
+
+	var $still = $('#ryu-still');
+	var $ready = $('#ryu-ready');
+	var $throwing = $('#ryu-throwing');
+	var $cool = $('#ryu-cool'); 
+	
 	//selects ryu container
 	$('#ryu')
 	//when mouse enters 
 	//div#ryu, ryue goes into ready mode
 	.on('mouseenter', function(){
-		$(this).find('#ryu-still').hide();
-		$(this).find('#ryu-ready').show();
+		$(this).find($still).hide();
+		$(this).find($ready).show();
+		$(this).find($cool).hide();
 	})
 	//when mouse leaves
 	//ryu goes into still mode
 	//if ryu is in the middle of a throw (mouse depressed)
 	//hadouken will still animate but ryu will go into still mode
-	.on('mouseleave', function(){
-		$(this).find('#ryu-still').show();
-		$(this).find('#ryu-ready').hide();
-		$(this).find('#ryu-throwing').hide();
+	.on('mouseleave', function() {
+		$(this).find($still).show();
+		$(this).find($ready).hide();
+		$(this).find($throwing).hide();
+		$(this).find($cool).hide();
+	})
 	//when mouse is depressed
 	//ryu goes from ready mode to throwing mode
 	//hadouken throws; moves across screen; recalled
-	}).on('mousedown', function(){
+	.on('mousedown', function(){
 		playHadouken();
-		$(this).find('#ryu-ready').hide();
-		$(this).find('#ryu-throwing').show();
+		$(this).find($ready).hide();
+		$(this).find($throwing).show();
+		$(this).find($cool).hide();
 		$(this).closest('#dojo').find('#hadouken')
 		.finish()
 		.show()
@@ -36,10 +47,28 @@ $(document).ready(function(){
 	//when mouse is released
 	//ryu returns to ready mode
 	.on('mouseup', function(){
-		$(this).find('#ryu-ready').show();
-		$(this).find('#ryu-throwing').hide();
+		$(this).find($ready).show();
+		$(this).find($throwing).hide();
+		$(this).find($cool).hide();
+	});
+
+	$('body').on('keydown', function(event){
+		if (event.keyCode == 88) {
+			$(this).find($cool).show();
+			$(this).find($ready).hide();
+			$(this).find($still).hide();
+			$(this).find($throwing).hide();
+		} 
+	});
+	
+	$('body').on('keyup', function(event){
+		if (event.keyCode == 88) {
+			$(this).find($cool).hide();
+			$(this).find($ready).show();
+		}
 	});
 });
+
 
 function playHadouken () {
 	$('#hadouken-sound')[0].volume = 0.5;
@@ -47,10 +76,8 @@ function playHadouken () {
 	$('#hadouken-sound')[0].play();
 }
 
-	// $('#ryu').mouseenter(function(){
-	// 	$('#ryu-still').hide();
-	// 	$('#ryu-ready').show();
-	// }).mouseleave(function(){
-	// 	$('#ryu-still').show();
-	// 	$('#ryu-ready').hide();
-	// });
+function playStage () {
+	$('#stage')[0].volume = 1;
+	$('#stage')[0].load();
+	$('#stage')[0].play();
+}
