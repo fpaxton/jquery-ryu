92b0d3292959a6e94ce397f2ffa94fe49aed26a8
fpaxton <farispaxton@Faris-Paxtons-MacBook.local>
2014-06-19 13:07:06 -0500

    Fixed conflicting event handlers
    Used if statements to check if ‘x’ was being pressed. If ryuPoseOff is
    true then other event handlers will function normally. If ryuPoseOff is
    false then he will remain in pose and other events will not work.

--- js/app.js
+++ js/app.js
@@ -6,54 +6,68 @@ $(document).ready(function(){
 	var $throwing = $('#ryu-throwing');
 	var $cool = $('#ryu-cool'); 
 	
+	var ryuPoseOff = true;
+
 	//selects ryu container
+	
 	$('#ryu')
 	//when mouse enters 
 	//div#ryu, ryue goes into ready mode
 	.on('mouseenter', function(){
-		$(this).find($still).hide();
-		$(this).find($ready).show();
-		$(this).find($cool).hide();
+		if (ryuPoseOff) {
+			$(this).find($still).hide();
+			$(this).find($ready).show();
+			$(this).find($cool).hide();
+		}
 	})
 	//when mouse leaves
 	//ryu goes into still mode
 	//if ryu is in the middle of a throw (mouse depressed)
 	//hadouken will still animate but ryu will go into still mode
 	.on('mouseleave', function() {
-		$(this).find($still).show();
-		$(this).find($ready).hide();
-		$(this).find($throwing).hide();
-		$(this).find($cool).hide();
+		if (ryuPoseOff) {
+			$(this).find($still).show();
+			$(this).find($ready).hide();
+			$(this).find($throwing).hide();
+			$(this).find($cool).hide();
+		}
 	})
 	//when mouse is depressed
 	//ryu goes from ready mode to throwing mode
 	//hadouken throws; moves across screen; recalled
+	
+	
 	.on('mousedown', function(){
-		playHadouken();
-		$(this).find($ready).hide();
-		$(this).find($throwing).show();
-		$(this).find($cool).hide();
-		$(this).closest('#dojo').find('#hadouken')
-		.finish()
-		.show()
-		.animate({'left': '300px'},
-			500,
-			function() {
-				$(this).hide();
-				$(this).css('left', '-212px');
-			});
-
+		if (ryuPoseOff) {
+			playHadouken();
+			$(this).find($ready).hide();
+			$(this).find($throwing).show();
+			$(this).find($cool).hide();
+			$(this).closest('#dojo').find('#hadouken')
+			.finish()
+			.show()
+			.animate({'left': '300px'},
+				500,
+				function() {
+					$(this).hide();
+					$(this).css('left', '-212px');
+				});
+		}
 	})
+	
 	//when mouse is released
 	//ryu returns to ready mode
 	.on('mouseup', function(){
-		$(this).find($ready).show();
-		$(this).find($throwing).hide();
-		$(this).find($cool).hide();
+		if (ryuPoseOff) {
+			$(this).find($ready).show();
+			$(this).find($throwing).hide();
+			$(this).find($cool).hide();
+		}
 	});
 
 	$('body').on('keydown', function(event){
 		if (event.keyCode == 88) {
+			ryuPoseOff = false;
 			$(this).find($cool).show();
 			$(this).find($ready).hide();
 			$(this).find($still).hide();
@@ -63,6 +77,7 @@ $(document).ready(function(){
 	
 	$('body').on('keyup', function(event){
 		if (event.keyCode == 88) {
+			ryuPoseOff = true;
 			$(this).find($cool).hide();
 			$(this).find($ready).show();
 		}
